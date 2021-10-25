9549ed1e068ab1205eea4b6389b7a6e4e9aa64c1
schilamkur <91044775+schilamkur@users.noreply.github.com>
2021-10-24 20:55:35 -0700

    Add files via upload

---
+++ Wk 9 - Random Grade Generator/Wk 9 - Random Grade Generator/src/com/company/controllers/Main.java
@@ -0,0 +1,33 @@
+package com.company.controllers;
+
+import com.company.models.GetPercentage;
+import com.company.views.CmdLineView;
+
+
+public class Main {
+    public static  CmdLineView view;
+    public static void main(String[] args) {
+	// read student names from file
+        int score=0;
+        String grade = "A";
+        GetStudentNames getStudents = new GetStudentNames();
+        String[] studentNames = getStudents.getTheStudentNames();
+
+        GetPercentage genRandScore = new GetPercentage(score);
+
+        view = new CmdLineView("", score, grade);
+        view.printHeader();
+        for(int i = 0; i < studentNames.length; i++) {
+
+            // Random generator - assign score to student
+            score = genRandScore.getPercentage();
+            // Assign a grade
+            grade = genRandScore.getGrade(score);
+            //print name, score and grade
+            view = new CmdLineView(studentNames[i], score, grade);
+            view.printStudentInfo();
+
+        }
+
+    }
+}
